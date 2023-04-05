609478379a43be350a2f3d815008b1aa18b9b66a
Karem Ahmed <113023102+kareem11223344@users.noreply.github.com>
2023-04-06 00:42:28 +0200

    Add files via upload

---
+++ js/mine.js
@@ -0,0 +1,9 @@
+let image = document.querySelector(".image");
+let container = document.querySelector(".container");
+function phones(phon){
+    image.src = phon ; 
+}
+
+function colors(color){
+    container.style.background = color;
+}
